e3471981f90f5f8f15b718284406967725c02fac
sarahleightaal <270141828@yoobeestudent.ac.nz>
2022-08-18 08:47:07 +1200

    api setup ready to netlify

---
+++ src/api.js
@@ -0,0 +1,25 @@
+//main file
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const router = express.Router()
+router.get("/", (req, res) => {
+    res.send("User List")
+})
+router.post("/", (req, res) => {
+    res.send("Create New User")
+})
+router
+.route("/:id")
+.get((req, res) => {
+    res.send(`Get a User With ID: ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Update a User With ID: ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete a User With ID: ${req.params.id}`)
+})
+app.use("/.netlify/functions/api", router)
+module.exports.handler = serverless(app)
+
